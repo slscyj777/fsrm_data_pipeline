a9daf9809afbce0a6cc37ef6f204727e3c5bdcae
slscyj777 <shanelee4568@gmail.com>
2026-06-29 10:49:00 +0700

    updated toml and update utils

--- utils/clear_csv_data.py
+++ utils/clear_csv_data.py
@@ -2,14 +2,14 @@ import polars as pl
 from datetime import date
 
 today = date.today()
-day = 14
+day = 25
 target_date = date(today.year, today.month, day)
 
 df = pl.read_csv(
-    ".data/FSRM_consolidated_June_2026.csv", 
+    "data/FSRM_consolidated_June_2026.csv", 
     schema_overrides={"stock_date": pl.Date}
 )
 
 df = df.filter(pl.col("stock_date") != target_date)
 
-df.write_csv(".data/FSRM_consolidated_June_2026.csv")
+df.write_csv("data/FSRM_consolidated_June_2026.csv")
